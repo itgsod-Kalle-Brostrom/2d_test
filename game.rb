40982581df57a39a8f40410b4eb5936cc6d5e9c1
Kalle Broström <Kalle.brostrom@elev.it-gymnasiet.se>
2012-12-07 14:27:25 +0100

    Added code + Player picture

--- game.rb
+++ game.rb
@@ -1,1 +1,26 @@
 require 'chingu'
+
+class Game < Chingu::Window
+
+	def initialize
+		super # Anropar metoden med samma namn i superklassen
+		self.input = {esc: :exit}
+		Player.create
+	end
+end
+
+class Player < Chingu::GameObject
+
+	def setup
+		@x = 400
+		@y = 300
+		@image = Gosu::Image["Ship.png"]
+
+
+
+	end
+
+
+end
+
+Game.new.show
